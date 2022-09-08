d0e7253e943abf102a3670f69af221bc5df0b648
bagoviggo <bagoviggo@gmail.com>
2022-09-08 20:51:29 +0000

    4-puts using betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * header files
+ * int main()
+ * block
+ * done
+ */
+
+int main(void)
+{
+	char my_str[60];
+
+	strcpy(my_str, "Programming is like building a multilingual puzzle");
+	
+	puts(my_str);
+	return(0);
+}
