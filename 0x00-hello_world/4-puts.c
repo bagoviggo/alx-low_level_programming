1ea1aa84e22ff4b317a904cc7cdfa5668051ab58
bagoviggo <bagoviggo@gmail.com>
2022-09-09 10:06:49 +0300

    fix code 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  * main - main block
- *
  * Return: 0
  */
 int main(void)
 {
-	char my_str[60];
-		strcpy(my_str, "\"Programming is like building a multilingual puzzle\n");
-			puts(my_str);
-				return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
