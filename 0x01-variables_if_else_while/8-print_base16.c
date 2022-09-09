c9454b311201180d423b37ce16199110c27a4ac0
bagoviggo <bagoviggo@gmail.com>
2022-09-09 19:39:29 +0300

    8-print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
-
+#include <ctype.h>
 /**
- * main - Entry point
- * Return:0
+ * main- prints all single digits of base 16
+ *
+ * Return: return 0
  */
 int main(void)
 {
-	int i, y;
-	for (i = 0; i < 10; i++)
-	{
-		putchar (i + '0');
-	}
-	for (y = 'a'; y <= 'f'; y++)
-	{
-		putchar (y);
-	}
+	int digit;
 
-putchar('\n');
-return (0);
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit == '9')
+		{
+			digit = 'a';
+			for (; digit <= 'f'; digit++)
+			{
+				putchar(digit);
+			}
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
 }
