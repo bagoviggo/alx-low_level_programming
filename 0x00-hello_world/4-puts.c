b7255fc8c49cbe7ff9afb7a1c90125d1beff246c
bagoviggo <bagoviggo@gmail.com>
2022-09-09 09:59:03 +0300

    fix newline in 4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	char my_str[60];
-		strcpy(my_str, "\"Programming is like building a multilingual puzzle");
+		strcpy(my_str, "\"Programming is like building a multilingual puzzle\n");
 			puts(my_str);
 				return (0);
 }
