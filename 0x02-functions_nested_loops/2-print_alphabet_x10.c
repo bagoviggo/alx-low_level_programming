bea1258b93c2eaa90c3f7b23465d665069f0308a
bagoviggo <bagoviggo@gmail.com>
2022-09-13 20:31:33 +0300

    2-alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - entry point
+ *Description: Prints alphabet ten times with nested for loop
+ *
+ *Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	char d;
+
+	for (d = 0; d < 10; d++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
