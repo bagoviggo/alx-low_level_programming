91320272e452e58d2c4dd7cd00d97b9c5d948fb3
bagoviggo <bagoviggo@gmail.com>
2022-09-19 15:53:18 +0300

    puts, print string~

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_puts - writes a sting to the stdout
+*@str: pointer for string
+*/
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
