e18e4af9e235e787cfb06571fa726aac314a4129
bagoviggo <bagoviggo@gmail.com>
2022-09-19 22:06:21 +0300

    8 print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include  "main.h"
+/**
+*print_array - prints n elements of an arr of int
+*@n: address for int type
+*@a: pointer for array
+*Description: numbers are to be separated by commas and space
+*Numbers to be displayed same as they are in the array
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,7 +10,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-
+void print_array(int *a, int n);
 
 
 #endif
