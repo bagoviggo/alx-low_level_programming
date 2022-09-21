987bcb18ffa1d9c087601c6820b89841ae27bf9a
bagoviggo <bagoviggo@gmail.com>
2022-09-22 02:34:50 +0300

    reverse array elements

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*reverse_array - func to reverse elements of array
+*@a: integer array to be reversed
+*@n:the number of elements of the array
+*
+*Return: void
+*/
+void reverse_array(int *a, int n)
+{
+	int i = 0, j;
+
+
+	n = n -1;
+	while (i < n)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+		i++;
+		n--;
+	}
+}
