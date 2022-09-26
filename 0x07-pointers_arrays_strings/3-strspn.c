4a1423aea2324e9477329d3a10ec52ed85941917
bagoviggo <bagoviggo@gmail.com>
2022-09-26 23:10:59 +0300

    length of prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - gets int length of a prefix substring
+ * @s: pointer to string to be checked
+ * @accept: string that gets checked
+ *
+ * Return: number of bytes of s in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,11 +3,8 @@
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-<<<<<<< HEAD
 char *_strchr(char *s, char c);
-=======
-
->>>>>>> b51d52db19128227abb1f5fadcb0fc541d558cee
+unsigned int _strspn(char *s, char *accept);
 
 
 
