ae8b8f3dd103e861e9588a4c3e1043fa38efaad4
bagoviggo <bagoviggo@gmail.com>
2022-10-11 12:36:56 +0300

    add struct description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - data type for dog
+*@name: first member //dog's name
+*@age: second member //dogs age
+*@owner: third member //dogs owner
+*/
 struct dog
 {
 	char *name;
