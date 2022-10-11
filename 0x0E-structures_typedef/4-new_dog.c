b2a7b27648eb29a647fd92e60fb3f90cefe6d882
bagoviggo <bagoviggo@gmail.com>
2022-10-11 20:58:08 +0300

    fix 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int n1, n2 , i;
+	unsigned int n1, n2, i;
 	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
@@ -20,7 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	
+
 	for (n1 = 0; name[n1]; n1++)
 		;
 	n1++;
