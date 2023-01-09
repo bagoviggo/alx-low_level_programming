c379201f701cfa0e2e783561205c084c78980b0d
bagoviggo <bagoviggo@pop-os.localdomain>
2023-01-09 23:28:10 +0300

    fix check 6 on 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,8 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (hash_table == NULL)
 		return (NULL);
+
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (hash_table->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
