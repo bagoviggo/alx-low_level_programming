96a4d958e45e97a4cede34bda9794cdcb10fe133
bagoviggo <bagoviggo@pop-os.localdomain>
2023-01-09 22:15:34 +0300

    fix betty 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,16 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned long int i;
+	unsigned long int i;
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
-    hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-        return NULL;
-    hash_table->size = size;
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (hash_table->array == NULL)
-        return NULL;
-    for (i = 0; i < size; i++)
-        hash_table->array[i] = NULL;
-    return hash_table;
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
